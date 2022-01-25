391d67bd9fbe2c214408a14925f0f4affa3e7544
Alex M. Ascensión <alexmascension@gmail.com>
2022-01-25 12:17:56 +0100

    make plot_adata_cluster_properties with metadata

--- fb_functions.py
+++ fb_functions.py
@@ -3,38 +3,39 @@ import numpy as np
 import scanpy as sc
 
 
-def plot_adata_cluster_properties(dict_cats_clusters={}, dict_datasets={}, what='presence', cluster_name='cluster', axis_name='axis'):
+def plot_adata_cluster_properties(dict_cats_clusters={}, list_datasets=[], what='presence', cluster_name='cluster', axis_name='axis'):
     df_cols = [i for i in list(dict_cats_clusters.keys()) if len(i) == 2]
-    df_index = ['-'.join(i[:-1]) for i in dict_datasets.values()]
+    df_index = []
+    for adata in list_datasets:
+        df_index.append(f"{adata.obs['Author'].values[0]}-{int(adata.obs['Year'].values[0])}-{'/'.join(adata.obs['Condition'].cat.categories)}")
+    
     df_clusters = pd.DataFrame('', index=df_index, columns=df_cols)
     
     
-    for adata_name, list_adata in dict_datasets.items():
-        adata = list_adata[-1]
-        list_adata = list_adata[:-1]
+    for adata, name_dataset in zip(list_datasets, df_index):
         for cluster in df_cols:
             if what == 'presence':
                 if cluster in set(adata.obs[cluster_name]):
-                    df_clusters.loc['-'.join(list_adata), cluster] = '✔️'
+                    df_clusters.loc[name_dataset, cluster] = '✔️'
                     
             if what == 'score':
                 if cluster in set(adata.obs[cluster_name]):
                     score = adata[adata.obs[cluster_name] == cluster].obs[f'{cluster_name}_{cluster}'].values
-                    df_clusters.loc['-'.join(list_adata), cluster] = f'{np.mean(score):.2f}'
+                    df_clusters.loc[name_dataset, cluster] = f'{np.mean(score):.2f}'
                 else:
-                    df_clusters.loc['-'.join(list_adata), cluster] = 0
+                    df_clusters.loc[name_dataset, cluster] = 0
             
             if what == 'percentage':
                 if cluster in set(adata.obs[cluster_name]):
                     percentage = len(adata[adata.obs[cluster_name] == cluster])/len(adata)
-                    df_clusters.loc['-'.join(list_adata), cluster] = f'{percentage:.2f}'
+                    df_clusters.loc[name_dataset, cluster] = f'{percentage:.2f}'
                 else:
-                    df_clusters.loc['-'.join(list_adata), cluster] = 0
+                    df_clusters.loc[name_dataset, cluster] = 0
                     
             if what == 'axis':
                 if cluster in set(adata.obs[cluster_name]):
                     axis_val = adata[adata.obs[cluster_name] == cluster].obs[axis_name].values[0]
-                    df_clusters.loc['-'.join(list_adata), cluster] = axis_val
+                    df_clusters.loc[name_dataset, cluster] = axis_val
                     
     return df_clusters
 
